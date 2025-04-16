71e9ed539bf73b55a9a0f345ebb4b0dbd0e94438
nightintoxicated <50459012+nightintoxicated@users.noreply.github.com>
2025-04-16 15:39:43 +0100

    Update loggy.pm

--- loggy.pm
+++ loggy.pm
@@ -8,12 +8,12 @@ sub loggy {
   my $dir = "loggy_logs";
   die "directory loggy_logs doesnt exist" unless -d $dir;
 
-  open (WFH, '>>', "$dir/$logfile.log") or die "LOGGY: could not open log file for $logfile.log\n";
+  open (LOGGYWFH, '>>', "$dir/$logfile.log") or die "LOGGY: could not open log file for $logfile.log\n";
   my @passed = @_;
   foreach my $item (@passed) {
-    print WFH "$description: $item\n";
+    print LOGGYWFH "$description: $item\n";
   }
-  print WFH "--------------\n";
+  print LOGGYWFH "--------------\n";
 }
-  close(WFH);
+  close(LOGGYWFH);
 1;
